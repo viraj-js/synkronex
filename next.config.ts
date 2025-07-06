ee2f14590c3ca6b856c80188ae87d6d932c63eb8
G.mota <virajj97112@gmail.com>
2025-07-06 20:34:02 +0530

    feat : added pwa using next-pwa

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+})
+
+const nextConfig: NextConfig = withPWA({
   /* config options here */
-};
+});
 
 export default nextConfig;
