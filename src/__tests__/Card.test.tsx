e675d46c9dcfdbfc1e863e1ec5228e87a778e56a
G.mota <virajj97112@gmail.com>
2025-07-14 22:11:18 +0530

    chore : rearrange the test folders

--- src/__tests__/Card.test.tsx
+++ src/__tests__/Card.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { Card } from '../Card';
+import { Card } from '../components/Card';
 
 describe('Card', () => {
   it('renders children', () => {
